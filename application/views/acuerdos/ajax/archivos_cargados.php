73204b677668fd705fa0d4a7a2bff8a61f782387
Raymundo Dominguez <raymundo.dominguez@correo.setab.gob.mx>
2021-04-12 15:30:41 -0500

    Corrección de ruta

--- application/views/acuerdos/ajax/archivos_cargados.php
+++ application/views/acuerdos/ajax/archivos_cargados.php
@@ -2,7 +2,7 @@
     $seguimiento_id = 0; 
     $folio          = 1;
     $ejercicio      = date('Y');
-    $uploadFolder   = ( $acuerdo_id )? FCPATH . "uploads/Acuerdos/{$ejercicio}/{$acuerdo_id}": FCPATH . "uploads/Acuerdos/{$ejercicio}";
+    $uploadFolder   = "uploads/Acuerdos/{$ejercicio}/{$acuerdo_id}";
 ?>
 <?php foreach ($archivos as $key => $archivo): ?>
     <?php if ( $archivo['seguimiento_acuerdo_id'] != $seguimiento_id ): ?>
